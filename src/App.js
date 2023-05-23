6187b32f21fba561ca8f9b43f2406793fd3297ed
ranjithkumar404 <ranjithkumarshetty808@gmail.com>
2023-05-24 00:21:23 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import item from './data';
+import Category from "./Category";
+import Menu from "./Menu";
 function App() {
+  const [mitem,setMitem]=useState(item);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+          <h1>Menu</h1>
+          <div>
+          {
+            mitem.map((i)=>{
+              const {id,desc,title,img,price,category}=i;
+              return (
+               <div key={id}>
+                <img className="w-[100px]" src={img} alt="" />
+                <p>{title}</p>
+                <p>${price}</p>
+                <p>{desc}</p>
+               </div>
+              )
+            })
+          }
+          </div>
+        </div>
     </div>
   );
 }
